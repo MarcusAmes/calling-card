ba5f36d3e213d6d0c61f1ee067ec138158c1d87a
Marcus Ames <marcusames00@gmail.com>
2019-01-20 11:53:52 -0700

    tests complete

--- src/main/java/com/galvanize/CellPhone.java
+++ src/main/java/com/galvanize/CellPhone.java
@@ -2,8 +2,65 @@ package com.galvanize;
 
 public class CellPhone {
     private CallingCard card;
+    private boolean isTalking = false;
+    private String callHistory = "";
+    private String callingPhoneNumber = "";
+    private int callTime = 0;
 
     public CellPhone(CallingCard card) {
         this.card = card;
+    }
+
+    @Override
+    public String toString() {
+        return "CellPhone{" +
+                "card=" + card +
+                ", isTalking=" + isTalking +
+                ", callHistory='" + callHistory + '\'' +
+                ", callingPhoneNumber='" + callingPhoneNumber + '\'' +
+                ", callTime=" + callTime +
+                '}';
+    }
+
+    public void call(String phoneNumber) {
+        callingPhoneNumber = phoneNumber;
+        isTalking = true;
+    }
+
+    public void endCall() {
+        if(callHistory.length() > 0) {
+            callHistory += ", ";
+        }
+        callHistory += callingPhoneNumber + " (";
+        if(card.getRemainingMinutes() <= 1) {
+            callHistory += "cut off at ";
+        }
+        if (callTime > 1) {
+            callHistory += callTime + " minutes)";
+        } else {
+            callHistory += callTime + " minute)";
+        }
+        isTalking = false;
+        callTime = 0;
+    }
+
+    public boolean isTalking() {
+        return isTalking;
+    }
+
+    public String getCallHistory() {
+        return callHistory;
+    }
+
+    public String getCallingPhoneNumber() {
+        return callingPhoneNumber;
+    }
+
+    public void tick() {
+        callTime++;
+        if(card.getRemainingMinutes() <= 1) {
+            endCall();
+        }
+        card.useMinutes(1);
     }
 }
--- src/test/java/com/galvanize/CellPhoneTest.java
+++ src/test/java/com/galvanize/CellPhoneTest.java
@@ -2,9 +2,105 @@ package com.galvanize;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CellPhoneTest {
     @Test
     public void InstanceDeclaration() {
         CellPhone cellPhone = new CellPhone(new CallingCard(10));
+        cellPhone.call("123-4567");
+        assertEquals("123-4567", cellPhone.getCallingPhoneNumber());
+    }
+
+    @Test
+    public void isTalkingTest() {
+        CellPhone cellPhone = new CellPhone(new CallingCard(10));
+        assertEquals(false, cellPhone.isTalking());
+        cellPhone.call("123-4567");
+        assertEquals(true, cellPhone.isTalking());
+    }
+
+    @Test
+    public void tickTest() {
+        CallingCard card = new CallingCard(10);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.tick();
+        assertEquals(7, card.getRemainingMinutes());
+    }
+
+    @Test
+    public void endCallTest() {
+        CallingCard card = new CallingCard(10);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        assertEquals(true, cellPhone.isTalking());
+        cellPhone.tick();
+        cellPhone.endCall();
+        assertEquals(false, cellPhone.isTalking());
+    }
+
+    @Test
+    public void cutOffTest() {
+        CallingCard card = new CallingCard(20);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.tick();
+        assertEquals(true, cellPhone.isTalking());
+        cellPhone.tick();
+        assertEquals(false, cellPhone.isTalking());
+        assertEquals(0, card.getRemainingMinutes());
+    }
+
+    @Test
+    public void callHistoryTest() {
+        CallingCard card = new CallingCard(10);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        cellPhone.endCall();
+        assertEquals("123-4567 (1 minute)", cellPhone.getCallHistory());
+    }
+
+    @Test
+    public void callHistoryTestMultiple() {
+        CallingCard card = new CallingCard(10);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        cellPhone.endCall();
+        cellPhone.call("333-3333");
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.endCall();
+        assertEquals("123-4567 (1 minute), 333-3333 (3 minutes)", cellPhone.getCallHistory());
+    }
+
+    @Test
+    public void callHistoryCutOffTest() {
+        CallingCard card = new CallingCard(20);
+        card.addMinutes(1);
+        CellPhone cellPhone = new CellPhone(card);
+        cellPhone.call("123-4567");
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.endCall();
+        cellPhone.call("333-3333");
+        cellPhone.tick();
+        cellPhone.tick();
+        cellPhone.tick();
+        assertEquals("123-4567 (2 minutes), 333-3333 (cut off at 3 minutes)", cellPhone.getCallHistory());
     }
 }
